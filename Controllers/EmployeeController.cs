59294b194a7e09f88051d6a60816b84d1a6476ed
spr1234siva <sprchr@outlook.com>
2021-09-13 21:08:12 -0500

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        // GET: api/<EmployeeController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            List<string> values = new List<string>();
+
+            EmployeeRepository employeeRepository = new EmployeeRepository();
+            employeeRepository.Add();
+
+            return values;
+        }
+
+        // GET api/<EmployeeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EmployeeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<EmployeeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EmployeeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
